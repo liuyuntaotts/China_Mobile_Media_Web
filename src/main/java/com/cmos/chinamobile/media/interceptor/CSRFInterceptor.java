8cc7af63ba67a30238faf39d740a29c681ed74f2
zhukexin <zhukexin@zeppin.cn>
2016-06-24 18:22:32 +0800

    6.24

--- src/main/java/com/cmos/chinamobile/media/interceptor/CSRFInterceptor.java
+++ src/main/java/com/cmos/chinamobile/media/interceptor/CSRFInterceptor.java
@@ -1,9 +1,13 @@
 package com.cmos.chinamobile.media.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 
+import com.cmos.chinamobile.media.action.UserAction;
+import com.cmos.core.util.User;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
@@ -14,6 +18,16 @@ public class CSRFInterceptor extends AbstractInterceptor {
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
 		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpServletResponse response = ServletActionContext.getResponse();
+		HttpSession session = request.getSession();
+		
+		if(invocation.getAction().getClass() != UserAction.class){
+			User user = (User)session.getAttribute("usersession");
+			if(user==null){
+				response.getWriter().write("{\"status\":\"unlogin\"}");
+				return null;
+			}
+		}
 		String referer = request.getHeader("Referer");
 //		if(!referer.startsWith("http://localhost:8090/")){
 //			return null;
